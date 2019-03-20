c5f3c9a72d649a41357f8e4819bbfa3fcb341eb0
user@windriver.com <user@windriver.com>
2019-03-20 13:42:29 +0200

    Rearranged the formatting of the file

--- ex1.15.c
+++ ex1.15.c
@@ -11,38 +11,37 @@ void celsiustofahr(void);
 
 int main(void)
 {
-  int c;
+	int c;
 
-  printf("Temperature Conversion Table\n");
-  printf("1 - Fahrenheit to Celsius Conversion\n"); 
-  printf("2 - Celsius to Fahrenheit Conversion\n");
-  printf("- Enter your Choice\n");
+	printf("Temperature Conversion Table\n");
+	printf("1 - Fahrenheit to Celsius Conversion\n");
+	printf("2 - Celsius to Fahrenheit Conversion\n");
+	printf("- Enter your Choice\n");
 
-    c = getchar();
+	c = getchar();
 
-  
-    if( c == '1')
-      fahrtocelsius();
-    else if( c == '2')
-      celsiustofahr();
-    else 
-      printf("Invalid Choice\n");
-  return 0;
+	if (c == '1')
+		fahrtocelsius();
+	else if( c == '2')
+		celsiustofahr();
+	else
+		printf("Invalid Choice\n");
+return 0;
 }
 
 void fahrtocelsius()
 {
-  float fahr;
+	float fahr;
 
-  for(fahr=LOWER;fahr<=UPPER;fahr=fahr+STEP)
-    printf("%3.0f%6.1f\n",fahr,(5.0/9.0)*(fahr-32.0));
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+		printf("%3.0f%6.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
 }
 
 void celsiustofahr()
 {
-  float celsius;
-  
-  for(celsius=LOWER;celsius<=UPPER;celsius=celsius+STEP)
-    printf("%3.0f%6.1f\n",celsius,(9.0*celsius)/5.0+32);
+	float celsius;
+
+	for (celsius = LOWER;celsius<=UPPER;celsius=celsius+STEP)
+		printf("%3.0f%6.1f\n",celsius,(9.0*celsius)/5.0+32);
 }
 
