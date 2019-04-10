13dd88312beae6d1564365a924eca3e65a663c2a
user@windriver.com <user@windriver.com>
2019-04-10 15:09:14 +0300

    modified version of 8.2

--- ex8.2.c
+++ ex8.2.c
@@ -1,7 +1,6 @@
 #include <fcntl.h>
 #include <stdarg.h>
-// The functions available from stdio.h are implemented here.
-//#include <stdio.h>
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/syscall.h>
